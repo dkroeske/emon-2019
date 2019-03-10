b15e42633f1dac0f0995fca4e0c57a76cfb5f202
Diederich Kroeske <dkroeske@popd.nl>
2019-03-10 21:41:32 +0100

    Work in progress

--- src/app/services/power.service.ts
+++ src/app/services/power.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
 import {Observable} from 'rxjs';
-import {map, catchError, tap} from 'rxjs/operators';
+import {map} from 'rxjs/operators';
 import {PowerItem} from '../models/power.model';
 
 @Injectable({
