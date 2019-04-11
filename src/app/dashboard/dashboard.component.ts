72ce93a3e6d7aa874cdbd4d6b2c76b0b85e18635
Diederich Kroeske <dkroeske@popd.nl>
2019-04-11 22:13:52 +0200

    Code cleanup

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
-import {map} from 'rxjs/operators';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,25 +8,6 @@ import {map} from 'rxjs/operators';
 export class DashboardComponent implements OnInit {
 
   signature = '00000000d926eb43';
-
-  cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
-    map(({ matches }) => {
-
-      if (matches) {
-        return [
-          { title: 'ActualPower', component: 'ActualPower', cols: 3, rows: 1 },
-          { title: 'Series Today', component: 'PowerSeries', cols: 3, rows: 1 },
-        ];
-      }
-
-      return [
-        { title: 'ActualPower', component: 'ActualPower', cols: 1, rows: 1 },
-        { title: 'Series Today', component: 'PowerSeries', cols: 2, rows: 1 },
-      ];
-    })
-  );
-
-  constructor(private breakpointObserver: BreakpointObserver) {}
 
   ngOnInit() {
   }
